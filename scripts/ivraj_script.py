bcfeb851c988c85953a70154b26e479b5dbe0400
Ivraj Seerha <iseerha@college.harvard.edu>
2017-10-08 14:49:27 -0400

    adding ivraj_script.py

---
+++ scripts/ivraj_script.py
@@ -0,0 +1,21 @@
+#! /bin/usr/env python
+#-*- coding: utf-8 -*-
+"""Code to run super cool and complex AI, ML, cloud stuff
+"""
+
+from __future__ import print_function
+import time
+
+def super_cool_fun():
+    """Runs AI, ML, cloud scripts"""
+    # TODO: Add some AI, ML, cloud stuff here
+    print("Running super conplex AI, ML, cloud stuff...")
+    time.sleep(3)
+    print("Done. We've just hacked the mainframe.")
+
+def main():
+    """Runs print functions"""
+    super_cool_fun()
+
+if __name__ == "__main__":
+    main()
